9a16a612e2aec00ec33259611b9949408a366d21
Samuel Fialho <samuel.fialho@gmail.com>
2020-06-11 19:03:59 +0100

    typography

--- sketch.js
+++ sketch.js
@@ -3,8 +3,11 @@ const { lerp }  = require('canvas-sketch-util/math');
 const random   = require('canvas-sketch-util/random');
 const palettes = require('nice-color-palettes');
 
+random.setSeed(random.getRandomSeed());
+console.log(random.getSeed());
 const settings = {
-  dimensions: [ 3840, 2160 ]
+  suffix: random.getSeed(),
+  dimensions: [ 2160, 2160 ]
 };
 
 const sketch = () => {
@@ -13,18 +16,19 @@ const sketch = () => {
 
   const createGrid = () => {
     const points = [];
-    const count = 25;
+    const count = 20;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y /(count - 1);
-        const radius = Math.abs(random.noise2D(u, v));
+        const radius = Math.abs(random.noise2D(u, v) * 0.1);
         points.push({
           color: random.pick(["#F9F7E8", "#39304A", "#E1D89F", "#FB9F89", "#63C0AD"]),
           // color: random.pick(palette),
           radius,
           // radius: random.value() * 4,
-          radius: random.value(),
+          // radius: random.value(),
+          rotation: random.noise2D(u, v),
           position: [ u, v ]
         });
       }
@@ -32,28 +36,44 @@ const sketch = () => {
     return points;
   };
 
-  random.setSeed(12);
-  const points = createGrid().filter(() => random.value() > 0.2);
+  // random.setSeed(12);
+  const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 400;
 
   return ({ context, width, height }) => {
-    context.fillStyle = '#F9F7E8';
+    context.fillStyle = '#151515';
     context.fillRect(0, 0, width, height);
 
-    const sam = points.forEach(data => {
-      const { position, radius, color } = data;
+    points.forEach(data => {
+      const {
+        position,
+        radius,
+        color,
+        rotation
+      } = data;
+
       const [ u, v ] = position;
-      console.log(color);
+
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v)
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
       // context.fillStyle = color;
       // context.fill();
-      context.strokeLine = 10;
-      context.strokeStyle = 'red';
-      context.stroke();
+
+      // context.strokeLine = 10;
+      // context.strokeStyle = 'red';
+      // context.stroke();
+
+      context.save();
+
+      context.fillStyle = color;
+      context.font = `${radius * width}px "Arial"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('.', 0, 0);
+      context.restore();
     });
   };
 };
